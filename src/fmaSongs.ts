cca699de8fc064cbd0c0925712a7e3c3c9ba2614
Jim Nelin <jim@jine.se>
2026-01-14 16:55:38 +0100

    Commit after the end of the day :) hey! it works, now i need a few more eventlisteneres to make it pop.
    Things to add:
    * Update app with cover art and song info (based upon index)
    * Update playing/pause icons according to status
    * Update "Spela" button when you press it ( to spelar ... )

---
+++ src/fmaSongs.ts
@@ -0,0 +1,166 @@
+
+// Need export, because we want to import Songs in other files
+export interface Song {
+    title: string; // Title of the song
+    artist: string; // Name of the artist
+    duration: string; // Duration in MM:SS format
+    file: string; // Link to the audio file
+    cover: string; // Link to the cover image
+    fmaLink: string; // Link to the Free Music Archive page
+}
+
+// Sample songs data from Free Music Archive (FMA)
+export const Songs: Song[] = [
+   {
+      title: "Dark Ambient",
+      artist: "YuraSoop",
+      duration: "02:48",
+      file: "https://files.freemusicarchive.org/storage-freemusicarchive-org/tracks/6WcFNslGH6DmNMzK5yZawwbgKEq43qr5e74ciI29.mp3",
+      cover: "https://freemusicarchive.org/image/?file=track_image/qIEvcaXcFfLlImnWpYC7uoW7Tm3kcs0ZkENBzXOC.jpg&width=290&height=290&type=track",
+      fmaLink: "https://freemusicarchive.org/music/yurasoop/single/dark-ambient/"
+    },
+   {
+      title: "Beautiful Ambient",
+      artist: "YuraSoop",
+      duration: "02:10",
+      file: "https://files.freemusicarchive.org/storage-freemusicarchive-org/tracks/2086X7PAohVSnFbkbDvti2CQaGYrWR4WnFL8zX1t.mp3",
+      cover: "https://freemusicarchive.org/image/?file=track_image/2wmEcKY3ZM8y5SphXcZBDCNGs1yG2lDuUvywbjhz.jpg&width=290&height=290&type=track",
+      fmaLink: "https://freemusicarchive.org/music/yurasoop/single/beautiful-ambient/"
+    },
+   {
+     title: "New Age Beautiful Atmosphere.mp3",
+     artist: "YuraSoop",
+     duration: "03:48",
+     file: "https://files.freemusicarchive.org/storage-freemusicarchive-org/tracks/fNTv0RemqERZcw0wauYgwWRGjU48CTrPRlxrC9en.mp3",
+     cover: "https://freemusicarchive.org/image/?file=track_image/sKaeaF48Cac5uDmUjWoY4Hw0CafqxOXcsDBSUfOE.jpg&width=290&height=290&type=track",
+     fmaLink: "https://freemusicarchive.org/music/yurasoop/single/new-age-beautiful-atmospheremp3/"
+   },
+   {
+     title: "Drone Background Ambient.mp3",
+     artist: "YuraSoop",
+     duration: "01:58",
+     file: "https://files.freemusicarchive.org/storage-freemusicarchive-org/tracks/Knj0FLCoVier9I3zWO4viw0EgYE8zslgJRwGbd9l.mp3",
+     cover: "https://freemusicarchive.org/image/?file=track_image/98wghHWpZ0rEh9mfzQSwBaja9Bc89g7ibIrGvQZd.jpg&width=290&height=290&type=track",
+     fmaLink: "https://freemusicarchive.org/music/yurasoop/single/drone-background-ambientmp3/"
+   },
+   {
+     title: "Arras",
+     artist: "Musical Meanderings",
+     duration: "03:08",
+     file: "https://files.freemusicarchive.org/storage-freemusicarchive-org/tracks/nZxBCEU4UGcSWvhl0l3PMDyjoTLHIgAqQtR3XFfn.mp3",
+     cover: "https://freemusicarchive.org/img/default/album.jpg",
+     fmaLink: "https://freemusicarchive.org/music/musical-meanderings/single/arras/"
+     },
+   {
+     title: "Soft Inspiring Ambient",
+     artist: "YuraSoop",
+     duration: "02:03",
+     file: "https://files.freemusicarchive.org/storage-freemusicarchive-org/tracks/ULBnzDaiK1TVS4A3aTth2osICrduypBsDhDjUgqj.mp3",
+     cover: "https://freemusicarchive.org/image/?file=track_image/IFbVaSZXbZgTMP1TUDCKvdJQFLMyZS3dz7TRpwOu.jpg&width=290&height=290&type=track",
+     fmaLink: "https://freemusicarchive.org/music/yurasoop/single/soft-inspiring-ambient/"
+   },
+   {
+     title: "Lo-Fi Ambient Vibe.mp3",
+     artist: "Sound Of Freedom",
+     duration: "02:45",
+     file: "https://files.freemusicarchive.org/storage-freemusicarchive-org/tracks/RMCJ4YZKaLCK2unISeHksSNiGCvK3VQpQrecSpp0.mp3",
+     cover: "https://freemusicarchive.org/image/?file=track_image/O8MdvTbNDafyA5YFjcNTyEd2RpaaIPkDLQkMtV1t.jpg&width=290&height=290&type=track",
+     fmaLink: "https://freemusicarchive.org/music/sound-of-freedom/single/lo-fi-ambient-vibemp3/"
+   },
+    {
+       title: "Got 12 Brand New Notebooks",
+       artist: "Jangwa",
+       duration: "06:57",
+       file: "https://files.freemusicarchive.org/storage-freemusicarchive-org/tracks/uuTfvDS9o2vigjYGZ8mZdRHgAitBAaEZRxIjSIMs.mp3",
+       cover: "https://freemusicarchive.org/image/?file=album_image/PkO54URatAUGJxEYWQ9iHBSF2t4gqZnABemPutPO.jpg&width=290&height=290&type=album",
+       fmaLink: "https://freemusicarchive.org/music/Dilating_Times/celle-ep/got-12-brand-new-notebooks-on-my-shelf/"
+     },
+    {
+      title: "Ad rem (demo)",
+      artist: "Iliaque",
+      duration: "03:52",
+      file: "https://files.freemusicarchive.org/storage-freemusicarchive-org/tracks/vlPtnysmixmrRoxqVO4DnMNFRW81jKVaXEZ1kwaP.mp3",
+      cover: "https://freemusicarchive.org/image/?file=track_image/PxImNktQyVthjtvTPEnvgOPKM7m8TgabkS6MKaex.jpg&width=290&height=290&type=track",
+      fmaLink: "https://freemusicarchive.org/music/Iliaque/single/ad-rem-demo/"
+    },
+    {
+      title: "Free Yoga Music",
+      artist: "Happiness In Music",
+      duration: "03:30",
+      file: "https://files.freemusicarchive.org/storage-freemusicarchive-org/tracks/dsEnxefAdsx7PovifVrC9AM5Zp74A9wOZg3saBtB.mp3",
+      cover: "https://freemusicarchive.org/image/?file=track_image/DdflK8zQwLrNbcnHeslKdJIDrTKxrGuDG6eUpinT.png&width=290&height=290&type=track",
+      fmaLink: "https://freemusicarchive.org/music/happiness-in-music/ambient-2/free-yoga-music-1/"
+    },
+   {
+     title: "pentarmonic",
+     artist: "Marcelo Bourdieu",
+     duration: "02:50",
+     file: "https://files.freemusicarchive.org/storage-freemusicarchive-org/tracks/wQFDrGlxRLRFv9WBt5cXaXyxKmj3plPYzXRE3wt0.mp3",
+     cover: "https://freemusicarchive.org/image/?file=track_image/jdmUYFUdbKFEW2Y0NwaQhv7UegTioBAsFWLXafmC.jpg&width=290&height=290&type=track",
+     fmaLink: "https://freemusicarchive.org/music/marcelo-bourdieu/view-of-dream/pentarmonic/"
+     },
+   {
+     title: "Transmission",
+     artist: "1000 Handz",
+     duration: "02:30",
+     file: "https://files.freemusicarchive.org/storage-freemusicarchive-org/tracks/EBobTykFjiwYM9QcSnHWXy4F5W6JhuQGrfMZ4aQE.mp3",
+     cover: "https://freemusicarchive.org/image/?file=track_image/ABkSACDcsHpp767VY36jBZENi9m8HlXS7oqJSFCG.png&width=290&height=290&type=track",
+     fmaLink: "https://freemusicarchive.org/music/1000-handz/cc-by-free-to-use-melodic-rap-instrumentals/transmission/"
+     },
+  {
+    title: "Clouds (Ambient Background Music)",
+    artist: "Alex Productions",
+    duration: "02:45",
+      file: "https://files.freemusicarchive.org/storage-freemusicarchive-org/tracks/YqAtDTM2Mg7ZNSH6tK8HHHmevFHzjIpz1NxHPrlw.mp3",
+     cover: "https://freemusicarchive.org/image/?file=track_image/hSlrKDPxvoW3vt2iG0A03R1vKzCXicZayjKozjKQ.jpg&width=290&height=290&type=track",
+    fmaLink: "https://freemusicarchive.org/music/alex-productions/single/ambient-background-music-clouds/"
+  },
+   {
+     title: "Tuscan Sun",
+     artist: "Chad Crouch",
+     duration: "03:20",
+     file: "https://files.freemusicarchive.org/storage-freemusicarchive-org/music/ccCommunity/Chad_Crouch/Ambient_Atmospheres/Chad_Crouch_-_Tuscan_Sun.mp3",
+     cover: "https://freemusicarchive.org/image/?file=images/tracks/Track_-_20190416143020138&width=290&height=290&type=track",
+     fmaLink: "https://freemusicarchive.org/music/Chad_Crouch/Ambient_Atmospheres/Tuscan_Sun/"
+   },
+   {
+     title: "Ruby",
+     artist: "Chad Crouch",
+     duration: "02:25",
+     file: "https://files.freemusicarchive.org/storage-freemusicarchive-org/music/ccCommunity/Chad_Crouch/Ambient_Atmospheres/Chad_Crouch_-_Ruby.mp3",
+     cover: "https://freemusicarchive.org/image/?file=images/tracks/Track_-_20190416143005999&width=290&height=290&type=track",
+     fmaLink: "https://freemusicarchive.org/music/Chad_Crouch/Ambient_Atmospheres/Ruby/"
+   },
+   {
+     title: "Taut",
+     artist: "Chad Crouch",
+     duration: "03:15",
+     file: "https://files.freemusicarchive.org/storage-freemusicarchive-org/music/ccCommunity/Chad_Crouch/Ambient_Atmospheres/Chad_Crouch_-_Taut.mp3",
+     cover: "https://freemusicarchive.org/image/?file=images/tracks/Track_-_20190416143014450&width=290&height=290&type=track",
+     fmaLink: "https://freemusicarchive.org/music/Chad_Crouch/Ambient_Atmospheres/Taut/"
+     },
+   {
+     title: "This too shall pass",
+     artist: "Mark Lingard",
+     duration: "06:49",
+     file: "https://files.freemusicarchive.org/storage-freemusicarchive-org/tracks/K282ielK77q7Ik0NQoS98FUQfZ3H44nVwOmCsKjl.mp3",
+     cover: "https://freemusicarchive.org/image/?file=track_image/xdxk8woZSEHWcdZslJPvTMF6tnqhlM5ZwHKH4jRy.jpg&width=290&height=290&type=track",
+     fmaLink: "https://freemusicarchive.org/music/mark-lingard/single/this-too-shall-pass-1/"
+     },
+   {
+     title: "A Better World",
+     artist: "Amarent",
+     duration: "04:35",
+     file: "https://files.freemusicarchive.org/storage-freemusicarchive-org/tracks/kM6DxXK3pNKCVqemtvA4p1ZrAou88VKzrhrFnr1W.mp3",
+     cover: "https://freemusicarchive.org/index.php/image/?file=track_image/DBMPntQYReHwcicBO2mJNfwNm0cnJAdTC5SzzK9d.jpg&width=290&height=290&type=track",
+     fmaLink: "https://freemusicarchive.org/index.php/music/amarent/free-ambient-music/a-better-world/"
+   },
+   {
+     title: "Solar Plexus",
+     artist: "pat102",
+     duration: "06:13",
+     file: "https://files.freemusicarchive.org/storage-freemusicarchive-org/tracks/tkIAL5Hmaz1wLjCfwgLAdc6QgmX0XgSbwiX8cXdv.mp3",
+     cover: "https://freemusicarchive.org/image/?file=track_image/uVKnZFL4STf5Oe0oJeiGgF9J6v97MvPF2ltpjk91.png&width=290&height=290&type=track",
+     fmaLink: "https://freemusicarchive.org/music/pat102/single/solar-plexus/"
+   }
+ ];
---
+++ src/index.ts
@@ -0,0 +1,82 @@
+import { Songs } from './fmaSongs.js';
+
+type PlayerStatus = 'playing' | 'paused' | 'stopped';
+
+// Print total number of songs
+//console.log(`Total songs in playlist: ${Songs.length}`);
+
+// List all songs with details
+/*Songs.forEach((song, index) => {
+  console.log(`${song.title} av ${song.artist} (${song.duration})`);
+  console.log(`File URL: ${song.file}`);
+  console.log(`Cover URL: ${song.cover}`);
+  console.log(`FMA link: ${song.fmaLink}`);
+});*/
+
+const playlist = document.getElementById('app-playlist');
+if (!playlist) {
+    alert('Playlist element not found');
+}
+
+// Loop through songs
+Songs.forEach((song, index) => {
+    // Create list item
+    const playlistItem = document.createElement('div');
+    playlistItem.className = 'song-container';
+
+    playlistItem.innerHTML = `
+        <img src="${song.cover}" alt="Cover for ${song.title}" class="song-cover">
+        <div class="song-details">
+            <div class="song-meta">
+                <span class="song-title"><a href="${song.fmaLink}" target="_blank">${song.title}</a></span>
+                <span class="song-info">av ${song.artist} (${song.duration})</span>
+            </div>
+            <button class="play-btn" data-index="${index}">Spela</button>
+        </div>
+    `;
+
+    // Add event listener to play button
+    const playBtn = playlistItem.querySelector('.play-btn') as HTMLButtonElement;
+    playBtn.addEventListener('click', () => playSong(index));
+
+    playlist.appendChild(playlistItem);
+});
+
+
+
+// Actual music player variables and functions below
+
+let currentSongIndex = 0; // Start with the first song
+let audio = new Audio(); // HTML5 Audio object
+let playerStatus: PlayerStatus = 'stopped'; // Initial status
+
+function playSong(index: number) {
+    if (index < 0 || index >= Songs.length) {
+        alert('Invalid song index');
+        return;
+    }
+    const song = Songs[index]!;
+    audio.src = song.file;
+    audio.play();
+    playerStatus = 'playing';
+    console.log(`Playing: ${song.title} by ${song.artist}`);
+}
+
+function pauseSong() {
+    if (playerStatus === 'playing') {
+        audio.pause();
+        playerStatus = 'paused';
+        console.log('Playback paused');
+    }
+}
+
+function stopSong() {
+    audio.pause();
+    audio.currentTime = 0;
+    playerStatus = 'stopped';
+    console.log('Playback stopped');
+}
+
+//playSong(currentSongIndex);
+//pauseSong();
+//stopSong();
